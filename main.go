79247da7f42b2f981f1b94f84db1fde81c20850d
Rainforest <rainforestnick@gmail.com>
2020-12-19 15:33:59 +0800

    deploy to google cloud app engine

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
+	app.Get("/about", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, this is Rainforest 🌧🌲🌲")
+	})
 
-	app.Listen(":3000")
+	port := getPort()
+	app.Listen(fmt.Sprintf(":%s", port))
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	return port
 }
