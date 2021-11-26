3c2deca2236818e632cb316b6e8d8d95b7f307fd
Rainforest Cheng <r08521610@ntu.edu.tw>
2021-11-27 00:58:04 +0800

    [ADD] setup swagger

--- main.go
+++ main.go
@@ -8,16 +8,31 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+
+	swagger "github.com/arsmn/fiber-swagger/v2"
+	_ "github.com/rainforest-tools/personal_backend/docs"
 )
 
+// @title Rainforest Personal Backend
+// @version 0.1
+
+// @contact.name Rainforest
+// @contact.url http://rainforest.tools
+// @contact.email rainforestnick@gmail.com
+
+// @host localhost:3000
+// @BasePath /
 func main() {
 	app := fiber.New(fiber.Config{Prefork: true})
 	// middleware
 	app.Use(recover.New())
 	app.Use(cors.New())
-	app.Get("/about", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, this is Rainforest 🌧🌲🌲")
-	})
+
+	app.Get("/about", About)
+
+	// docs
+	app.Get("/swagger/*", swagger.Handler)
+	app.Get("/health", HealthCheck)
 
 	app.Static("/static", "./static")
 
@@ -33,4 +48,26 @@ func getPort() string {
 	}
 
 	return port
+}
+
+// About
+// @Summary About
+// @Router /about [get]
+// @Produce text/plain
+// @Success      200  {object}  string
+func About(c *fiber.Ctx) error {
+	return c.SendString("Hello, this is Rainforest 🌧🌲🌲")
+}
+
+// HealthCheck
+// @Summary Show the status of server
+// @Router /health [get]
+func HealthCheck(c *fiber.Ctx) error {
+	res := map[string]interface{}{
+		"data": "Server is up and running",
+	}
+	if err := c.JSON(res); err != nil {
+		return err
+	}
+	return nil
 }
